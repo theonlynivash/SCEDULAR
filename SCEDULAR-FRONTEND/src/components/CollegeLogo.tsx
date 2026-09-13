d4fc978ff9bd9f444be2149adbfabc454338ceb4
VLN_NIVASH <theonlynivash@gmail.com>
2026-09-13 15:05:55 +0530

    Use uploaded logo images in frontend

--- SCEDULAR-FRONTEND/src/components/CollegeLogo.tsx
+++ SCEDULAR-FRONTEND/src/components/CollegeLogo.tsx
@@ -1,7 +1,7 @@
 export default function CollegeLogo({ className = '' }: { className?: string }) {
   return (
     <img
-      src="/pec-logo.png"
+      src="/PEC-Logo-Updated-July-12-2024-218x150.png"
       alt="Panimalar Engineering College crest"
       className={`block object-contain aspect-square ${className}`}
     />
--- SCEDULAR-FRONTEND/src/components/ScedularLogo.tsx
+++ SCEDULAR-FRONTEND/src/components/ScedularLogo.tsx
@@ -1,3 +1,3 @@
 export default function ScedularLogo({ className = '' }: { className?: string }) {
-  return <img src="/scedular-logo.svg" alt="SCEDULAR timetable system" className={`block w-full max-w-[680px] h-auto aspect-[1098/388] ${className}`} />
+  return <img src="/scedular_logo.png" alt="SCEDULAR timetable system" className={`block w-full max-w-[680px] h-auto ${className}`} />
 }
